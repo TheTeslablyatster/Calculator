7c4a262369f61af4bc27c4930e97353b4a0cdd22
TheTeslablyatster <58738428+TheTeslablyatster@users.noreply.github.com>
2021-04-13 23:28:39 +0300

    Добавьте файлы проекта.

---
+++ Calculator/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿
+namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Num1Box = new System.Windows.Forms.TextBox();
+            this.Num2Box = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.AnswerBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(72, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Первое число";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(330, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Второе число";
+            // 
+            // textBox1
+            // 
+            this.Num1Box.Location = new System.Drawing.Point(75, 86);
+            this.Num1Box.Name = "textBox1";
+            this.Num1Box.Size = new System.Drawing.Size(129, 20);
+            this.Num1Box.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.Num2Box.Location = new System.Drawing.Point(333, 86);
+            this.Num2Box.Name = "textBox2";
+            this.Num2Box.Size = new System.Drawing.Size(129, 20);
+            this.Num2Box.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(225, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Операция";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/"});
+            this.comboBox1.Location = new System.Drawing.Point(228, 85);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(74, 21);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(75, 142);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(227, 37);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Посчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(397, 142);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(227, 37);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Очистить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.AnswerBox.Location = new System.Drawing.Point(495, 86);
+            this.AnswerBox.Name = "textBox3";
+            this.AnswerBox.Size = new System.Drawing.Size(129, 20);
+            this.AnswerBox.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(492, 44);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Результат";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(690, 211);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.AnswerBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Num2Box);
+            this.Controls.Add(this.Num1Box);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Num1Box;
+        private System.Windows.Forms.TextBox Num2Box;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox AnswerBox;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Calculator/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Num1Box.Text == "" 
+                || Num2Box.Text == "" 
+                || comboBox1.Text == ""
+                || double.TryParse(Num1Box.Text, out double result) == false
+                || double.TryParse(Num2Box.Text, out double result2) == false)
+                AnswerBox.Text = "Error";
+            else
+            {
+                double FirstNum = Convert.ToDouble(Num1Box.Text);
+                double SecondNum = Convert.ToDouble(Num2Box.Text);
+
+                switch (comboBox1.Text)
+                {
+                    case "+":
+                        AnswerBox.Text = Convert.ToString(FirstNum + SecondNum);
+                        break;
+                    case "-":
+                        AnswerBox.Text = Convert.ToString(FirstNum - SecondNum);
+                        break;
+                    case "*":
+                        AnswerBox.Text = Convert.ToString(FirstNum * SecondNum);
+                        break;
+                    case "/":
+                        if (SecondNum == 0)
+                            AnswerBox.Text = "Error";
+                        else
+                            AnswerBox.Text = Convert.ToString(FirstNum / SecondNum);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Num1Box.Text = null;
+            Num2Box.Text = null;
+            AnswerBox.Text = null;
+            comboBox1.Text = null;
+        }
+    }
+}
